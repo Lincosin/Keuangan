abbc3d60954ace241eafa9b1607a59ee8309c246
Lincosin <willynugraha129@gmail.com>
2026-01-19 16:12:11 +0800

    initial commit

--- Keuangan/src/main/java/com/mycompany/keuangan/Keuangan.java
+++ Keuangan/src/main/java/com/mycompany/keuangan/Keuangan.java
@@ -11,6 +11,6 @@ package com.mycompany.keuangan;
 public class Keuangan {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
